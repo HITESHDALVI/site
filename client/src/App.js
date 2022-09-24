22c9ab967b960e567228d645c32a09c40066d8fc
HITESHDALVI <70736293+HITESHDALVI@users.noreply.github.com>
2022-09-24 13:57:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,10 @@ import { Box } from '@mui/material';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import DetailView from './components/details/DetailView';
 import Cart from './components/cart/Cart';
-
+import About from './components/OtherPage/About';
+import ContactPage from './components/OtherPage/ContactPage';
+import FAQPage from './components/OtherPage/FAQPage';
+import ReturnPolicy from './components/OtherPage/ReturnPolicy';
 
 
 
@@ -22,6 +25,10 @@ function App() {
           <Route path= '/' element={<Home />} />
           <Route path= '/cart' element={<Cart />} />
           <Route path= '/product/:id' element={<DetailView />} />
+          <Route path= '/About' element={<About />} />
+          <Route path= '/ContactUs' element={<ContactPage />} />
+          <Route path= '/FAQ' element={<FAQPage />} />
+          <Route path= '/ReturnPolicy' element={<ReturnPolicy />} />
           </Routes>
         </Box>
       </BrowserRouter>
