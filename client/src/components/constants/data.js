e920649cfb93a037edef21d9c091e27c46966d98
HITESHDALVI <70736293+HITESHDALVI@users.noreply.github.com>
2022-10-02 12:27:38 +0530

    Update data.js

--- client/src/components/constants/data.js
+++ client/src/components/constants/data.js
@@ -1,10 +1,11 @@
 import homeAppliances from "../images/HomeAppliances.png";
+import furniturImg from '../images/furnitureImg3.png';
 export const navData = [
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100', text: 'Top Offers' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100', text: 'Mobile' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100', text: 'Fashion' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100', text: 'Electronics' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100', text: 'Home' },
+    { url: furniturImg, text: 'Home' },
     { url: homeAppliances, text: 'Appliances' },
     // { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' }
 ];
