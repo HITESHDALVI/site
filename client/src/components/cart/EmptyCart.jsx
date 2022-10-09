b01de84ced1aee0f03e2b010730c5bb31850486c
HITESHDALVI <70736293+HITESHDALVI@users.noreply.github.com>
2022-10-09 17:41:29 +0530

    Update EmptyCart.jsx
    changes in image and size of image

--- client/src/components/cart/EmptyCart.jsx
+++ client/src/components/cart/EmptyCart.jsx
@@ -1,4 +1,5 @@
 import { Typography, Box, styled } from '@mui/material';
+import EmptyCartImg from "../images/EmptyCartImg.jpg"
 
 const Component = styled(Box)`
     width: 80%%;
@@ -13,17 +14,16 @@ const Container = styled(Box)`
 `;
 
 const Image = styled('img')({
-    width: '15%'
+    width: '25%'
 });
 
 
 const EmptyCart = () => {
-    const imgurl = 'https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90';
     
     return (
         <Component>
             <Container>
-                <Image src={imgurl} />
+                <Image src={EmptyCartImg} />
                 <Typography>Your cart is empty!</Typography>
                 <Typography component="span">Add items to it now.</Typography>
             </Container>
